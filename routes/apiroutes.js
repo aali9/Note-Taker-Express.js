0913fb07bda81e158132de2a792aa2549e951e61
aali9 <74592306+aali9@users.noreply.github.com>
2022-06-25 16:53:31 +0100

    add note to array of objects in db.json

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,6 +5,16 @@ const uniqueid = require ('uniqueid')
 router.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf8', (err, data)=>{
         if (err) throw err
+        return res.json(JSON.parse(data))
+
+    })
+
+})
+// adds note to arrays of objects in db.json
+
+router.get('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data)=>{
+        if (err) throw err
         const db = JSON.parse(data)
         req.body.id = uniqueid()
         db.push(req.body)
@@ -14,3 +24,4 @@ router.get('/api/notes', (req, res) => {
         })
     })
 })
+
