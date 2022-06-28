109d711ae2715b9b9bea57d1ec568a4414ee935c
aali9 <74592306+aali9@users.noreply.github.com>
2022-06-28 17:50:33 +0100

    add note works

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,49 +1,46 @@
-const router = require ('express').Router()
-const fs = require ('fs')
-const uniqueid = require ('uniqueid')
+const router = require("express").Router();
+const fs = require("fs");
+const uniqueid = require("uniqueid");
 
-router.get('/api/notes', (req, res) => {
-    fs.readFile('db/db.json', 'utf8', (err, data)=>{
-        if (err) throw err
-        return res.json(JSON.parse(data))
-
-    })
-
-})
+router.get("/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    return res.json(JSON.parse(data));
+  });
+});
 // adds note to arrays of objects in db.json
 
-router.get('/api/notes', (req, res) => {
-    fs.readFile('db/db.json', 'utf8', (err, data)=>{
-        if (err) throw err
-        const db = JSON.parse(data)
-        req.body.id = uniqueid()
-        db.push(req.body)
-        fs.writeFile('db/db.json',JSON.stringify(db), (err)=>{
-            if (err) throw err 
-            res.json (db)
-        })
-    })
-})
+router.post("/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const db = JSON.parse(data);
+    req.body.id = uniqueid();
+    db.push(req.body);
+    fs.writeFile("db/db.json", JSON.stringify(db), (err) => {
+      if (err) throw err;
+      res.json(db);
+    });
+  });
+});
 
 // remove from array of objects
 
-   router.delete('/api/notes/:id', (req, res)=>{
-    fs.readFile('db/db.json', 'utf8', (err,data)=>{
-        if (err)throw err
-        const dbData = JSON.parse(data)
-        const noteId = req.params.id
+router.delete("/notes/:id", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const dbData = JSON.parse(data);
+    const noteId = req.params.id;
 
-        for (let i=0 ;i<dbData.length; i++){
-            if(noteId ===dbData[i].id){
-                dbData.splice([i],1)
-                fs.writeFile('db/db.json', JSON.stringify (db), (err)=>{
-                    if (err) throw err
-                    res.json (db)
-                })
-            }
-        }
-    })
+    for (let i = 0; i < dbData.length; i++) {
+      if (noteId === dbData[i].id) {
+        dbData.splice([i], 1);
+        fs.writeFile("db/db.json", JSON.stringify(db), (err) => {
+          if (err) throw err;
+          res.json(db);
+        });
+      }
+    }
+  });
+});
 
-   })
-
-   module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ('express')
 const homeRoutes = require ('./routes/homeroutes')
 const apiRoutes = require ('./routes/apiroutes')
+const path = require("path")
 
 const PORT = process.env.PORT || 3001;
 const app= express()
@@ -10,8 +11,17 @@ const app= express()
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use(apiRoutes);
-app.use(homeRoutes);
+app.use('/api',apiRoutes);
+// app.use('/api',homeRoutes);
+
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
 
 //fires server into Port3001 or browser
 
