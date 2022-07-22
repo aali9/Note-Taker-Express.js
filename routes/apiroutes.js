49e8c16ce2d5613bd85089ce851368eea3941714
aali9 <74592306+aali9@users.noreply.github.com>
2022-07-22 21:55:33 +0100

    changes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,38 +1,53 @@
-const express = require ("express");
-const router = express.Router();
-const fs = require("fs");
-const uuid = require("uuid");
+const express= require("express")
+const router = express.Router()
+const fs=require("fs")
+const uniqid=require("uuid")
 
 
-/* Get function to return data. */
-router.get("/api/notes", (req, res) => {
-    fs.readFile("db/db.json", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        return res.json(JSON.parse(data));
-      }
-    });
-  });
-  
-/* Post function to add new notes. */
-router.post("/api/notes", (req, res) => {
-    const note = JSON.parse(fs.readFileSync("db/db.json"));
-    const newNote = req.body;
-    newNote.id = uuid.v4();
-    note.push(newNote);
-    fs.writeFileSync("db/db.json", JSON.stringify(note));
-    res.json(note);
-  });
-  
-  /* Delete method to remove notes.*/
-  router.delete("/api/notes/:id", (req, res) => {
-    const note = JSON.parse(fs.readFileSync("db/db.json"));
-    const deleteNote = note.filter(
-      (removeNote) => removeNote.id !== req.params.id
-    );
-    fs.writeFileSync("db/db.json", JSON.stringify(deleteNote));
-    res.json(deleteNote);
-  });
-  
-  module.exports = router;
+router.get("/api/notes", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err,data)=>{
+        if(err) throw err
+        return res.json(JSON.parse(data))
+    })
+})
+
+//creates a note and adds it to the array of objects in db.json
+router.post("/api/notes", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err, data)=>{
+        if(err) throw err;
+        const dbData = JSON.parse(data)
+        const newNote= req.body
+        const id= "id"
+        const noteId = uuid()
+        newNote[id] = noteId
+        dbData.push(newNote)
+
+        fs.writeFile("db/db.json", JSON.stringify(dbData), (err)=>{
+            if(err) throw err
+            return res.json(dbData)
+        })
+    })
+} )
+
+//removes it from the array of objects
+router.delete("/api/notes/:id", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err,data)=>{
+        if(err) throw err;
+        const dbData = JSON.parse(data)
+        const noteId = req.params.id
+
+        for(let i=0 ;i<dbData.length; i++){
+            if(noteId ===dbData[i].id){
+                dbData.splice([i],1)
+                fs.writeFile("db/db.json", JSON.stringify(dbData), (err)=>{
+                    if(err) throw err
+                    return res.json(dbData)
+                }) 
+            }
+        }
+          
+    })
+})
+
+
+module.exports= router;
--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -1,14 +1,14 @@
-const express= require("express");
-const router = express.Router();
-const path = require("path");
+const express= require("express")
+const router = express.Router()
+const path = require("path")
 
 router.get("/notes", (req, res)=>{
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+})
 
 
 router.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
 
 module.exports = router;
